82339126fd8aad294e843678d4723d9e951594b2
kris <kristenjacobs@hotmail.com>
2013-01-27 18:55:18 +0000

    Added the expected test file for the validators test

--- tests/Validators/test.py
+++ tests/Validators/test.py
@@ -4,5 +4,9 @@ sys.path.append("../")
 import testutils
 
 testutils.run("ant > bin/build.txt 2>&1")
-testutils.run("java -jar test.jar testBoolean 12 -exit > bin/output.txt")
+
+testutils.run("echo \"===== Boolean tests =====\" >> bin/output.txt")
+testutils.run("java -jar test.jar testBoolean true -exit >> bin/output.txt")
+testutils.run("java -jar test.jar testBoolean false -exit >> bin/output.txt")
+testutils.run("java -jar test.jar testBoolean invalid -exit >> bin/output.txt")
 
