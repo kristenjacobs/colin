c3f7756d00a5d0dad9416d73ab5f1b3c2b36d349
kris <kristenjacobs@hotmail.com>
2013-01-28 19:09:12 +0000

    more test tidying

--- jcolin/src/jcolin/validators/BooleanValidator.java
+++ jcolin/src/jcolin/validators/BooleanValidator.java
@@ -4,7 +4,7 @@ public class BooleanValidator implements Validator {
 
 	public boolean validate(String value) {
 		String lower = value.toLowerCase();
-		return ((lower == "true") || 
-			    (lower == "false"));	
+		return ((lower.equals("true")) || 
+			    (lower.equals("false")));	
 	}
 }
--- tests/Validators/test.py
+++ tests/Validators/test.py
@@ -6,7 +6,7 @@ import testutils
 testutils.run("ant > bin/build.txt 2>&1")
 
 testutils.run("echo \"===== Boolean tests =====\" >> bin/output.txt")
-testutils.run("java -jar test.jar testBoolean true -exit >> bin/output.txt")
-testutils.run("java -jar test.jar testBoolean false -exit >> bin/output.txt")
-testutils.run("java -jar test.jar testBoolean invalid -exit >> bin/output.txt")
+testutils.run("java -jar bin/test.jar testBoolean true -exit >> bin/output.txt")
+testutils.run("java -jar bin/test.jar testBoolean false -exit >> bin/output.txt")
+testutils.run("java -jar bin/test.jar testBoolean invalid -exit >> bin/output.txt")
 
