c5aeb150c6da86a801f72f017df8d44b5602b048
kris <kristenjacobs@hotmail.com>
2013-01-28 21:30:42 +0000

    Added required file to the validator mechanism

---
+++ jcolin/src/jcolin/validators/ValidatorAttribute.java
@@ -0,0 +1,11 @@
+package jcolin.validators;
+
+public class ValidatorAttribute {
+    String m_name;
+    String m_value;
+    
+    ValidatorAttribute(String name, String value) {
+    	m_name = name;
+    	m_value = value;    		
+    }
+}
