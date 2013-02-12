e19b994eda84c82b7c8a34035ef44119389956a4
kris <kristenjacobs@hotmail.com>
2013-02-12 21:09:11 +0000

    Fixed test issue

--- tests/SourceInternal/test.py
+++ tests/SourceInternal/test.py
@@ -3,7 +3,7 @@ import os
 sys.path.append("../")
 import testutils
 
-os.environ['PYTHON_HOME'] = os.getcwd() + "/../../Implemantation/jcolin/jython" 
+os.environ['PYTHON_HOME'] = os.getcwd() + "/../../jcolin/jython" 
 os.environ['PYTHON_VERBOSE'] = 'error' 
 
 testutils.run(testutils.jcolin() + " source cmds.py arg1 arg2 arg3 -exit > bin/output.txt")
